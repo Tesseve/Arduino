1a54f6cf5bd19fad9227d75b183972774cf1a8d4
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-02-24 15:03:40 +0100

    fix middleware api

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\ApiKeyMiddleware::class
 
     ];
 
@@ -41,6 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\ApiKeyMiddleware::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
