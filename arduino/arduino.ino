fb180c36e25a2d780a9ad7d2bb869d45d3551697
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-03-02 12:15:09 +0100

    can play with screen

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,315 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Pushbutton.h>
+
+const int nbrOfButtons = 3;
+int nbrOfLeds = 3;
+int nbrRound = 20;
+int PUNISHMENT = 1000;
+
+int switchPin = 2;
+
+int pinButton1 = 1;
+int pinButton2 = 3;
+int pinButton3 = 4;
+int pinButton4 = 5;
+int pinButton5 = 6;
+int pinButton6 = 7;
+
+int buttonsPin1[] = {pinButton1, pinButton2, pinButton3};
+int buttonsPin2[] = {pinButton4, pinButton5, pinButton6};
+
+int pinLed1 = 8;
+int pinLed2 = 9;
+int pinLed3 = 10;
+int pinLed4 = 11;
+int pinLed5 = 12;
+int pinLed6 = 13;
+
+int leds1[] = {pinLed1, pinLed2, pinLed3};
+int leds2[] = {pinLed4, pinLed5, pinLed6};
+
+int randomLed1;
+int timeStart1 = 0;
+int timeEnd1 = 0;
+int nbrRoundPlayed1 = 0;
+int isGameOver1 = 0;
+int gamingLeds1[] = {-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,};
+int ledStarted1 = -1;
+
+int randomLed2;
+int timeStart2 = 0;
+int timeEnd2 = 0;
+int nbrRoundPlayed2 = 0;
+int isGameOver2 = 0;
+int gamingLeds2[] = {-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,-1, -1,};
+int ledStarted2 = -1;
+
+Pushbutton button1(pinButton1);
+Pushbutton button2(pinButton2);
+Pushbutton button3(pinButton3);
+Pushbutton button4(pinButton4);
+Pushbutton button5(pinButton5);
+Pushbutton button6(pinButton6);
+
+Pushbutton buttons1[] = {button1, button2, button3};
+Pushbutton buttons2[] = {button4, button5, button6};
+
+int winner = 0;
+
+int canPlay1 = 0;
+int canPlay2 = 0;
+
+int lastBlink1 = 0;
+int lastBlink2 = 0;
+
+int lastBlinkState1 = 0;
+int lastBlinkState2 = 0;
+
+int lastValueSwitch = 0;
+
+int on()
+{
+  return LOW;
+}
+
+int off()
+{
+  return HIGH;
+}
+
+void setup()
+{
+  
+
+  Serial.begin(9600);
+  randomSeed(analogRead(A5));
+
+  setupGame();
+
+  //startGame();
+}
+
+void loop()
+{
+
+  int switchValue = digitalRead(switchPin);
+  if(lastValueSwitch != switchValue) {
+    Serial.println(switchValue);
+    lastValueSwitch = switchValue;
+  }
+  /* readIncomingDatas();
+
+
+  if(canPlay1 == 1) {
+    startLeds1();
+    for (int i = 0; i < nbrOfButtons; i++)
+    {
+      if (buttons1[i].getSingleDebouncedPress())
+      {
+        checkIfWellPlayedPlayer1(i);
+      }
+    }
+  }
+
+  if(canPlay2 == 1) {
+    startLeds2();
+
+    for (int i = 0; i < nbrOfButtons; i++)
+    {
+      if (buttons2[i].getSingleDebouncedPress())
+      {
+        checkIfWellPlayedPlayer2(i);
+      }
+    }  
+  }
+
+  endGame(); */
+  
+}
+
+void checkIfWellPlayedPlayer1(int i) {
+  if(i == gamingLeds1[nbrRoundPlayed1]) {
+    nbrRoundPlayed1++;
+    return 1;
+  } else {
+    sendCommand("error:1");
+    return 0;
+  }
+}
+
+void checkIfWellPlayedPlayer2(int i) {
+  if(i == gamingLeds2[nbrRoundPlayed2]) {
+    nbrRoundPlayed2++;
+    return 1;
+  } else {
+    sendCommand("error:2");
+    return 0;
+  }
+}
+
+
+void setupGame()
+{
+
+  //set variables default
+  
+  canPlay1 = 0;
+  canPlay2 = 0;
+
+  lastBlink1 = 0;
+  lastBlink2 = 0;
+
+  lastBlinkState1 = 0;
+  lastBlinkState2 = 0;
+
+  nbrRoundPlayed1 = 0;
+  nbrRoundPlayed2 = 0;
+
+
+
+  //turn off the lights
+  for (int i = 0; i < nbrOfLeds; i++)
+  {
+    pinMode(leds1[i], OUTPUT);
+    pinMode(leds2[i], OUTPUT);
+
+    digitalWrite(leds1[i], off());
+    digitalWrite(leds2[i], off());
+  }
+
+  //set the lights to play for each player
+  for (int i = 0; i < nbrRound; i++)
+  {
+    int _randomLed1;
+    do
+    {
+      _randomLed1 = random(0, nbrOfLeds);
+    } while (_randomLed1 == randomLed1);
+    randomLed1 = _randomLed1;
+    gamingLeds1[i] = randomLed1;
+
+    int _randomLed2;
+    do
+    {
+      _randomLed2 = random(0, nbrOfLeds);
+    } while (_randomLed2 == randomLed2);
+    randomLed2 = _randomLed2;
+    gamingLeds2[i] = randomLed2;
+  }
+
+  /* for (int i = 0; i < nbrRound; i++)
+  {
+    Serial.print(gamingLeds1[i]);
+    Serial.print(" ");
+    Serial.println(gamingLeds2[i]);
+  } */
+  
+}
+
+void startLeds1() {
+  int led1 = gamingLeds1[nbrRoundPlayed1];
+  
+  if(ledStarted1 != led1) {
+    digitalWrite(leds1[ledStarted1], off());
+    digitalWrite(leds1[led1], on());
+    ledStarted1 = led1;
+  } 
+
+  
+}
+
+void startLeds2() {
+  int led2 = gamingLeds2[nbrRoundPlayed2];
+  if(ledStarted2 != led2) {
+      digitalWrite(leds2[ledStarted2], off());
+      digitalWrite(leds2[led2], on());
+      ledStarted2 = led2;
+  } 
+}
+
+void endGame() {
+  if(nbrRoundPlayed1 == nbrRound) {
+    winner += 1;
+    /* for(int j = 0; j < nbrOfLeds; j++) {
+      digitalWrite(leds1[j], off());
+    } */
+    if(canPlay1 == 1) {
+      sendCommand("stop:1");
+      blinkLightWinner1();
+    }
+    canPlay1 = 0;
+
+  } 
+  
+  if(nbrRoundPlayed2 == nbrRound) {
+    winner += 2;
+    /* for(int j = 0; j < nbrOfLeds; j++) {
+      digitalWrite(leds2[j], off());
+    } */
+    if(canPlay2 == 1) {
+      sendCommand("stop:2");
+      blinkLightWinner2();
+    }
+    canPlay2 = 0;
+  }
+}
+
+void blinkLightWinner1() {
+  /* for(int i = 0; i < 10; i++) {
+    if(millis() - lastBlink1 > 100 ) {
+      for(int j = 0; j < nbrOfLeds; j++) {
+        digitalWrite(leds1[j], lastBlinkState1 == 0 ? on() : off());
+      }
+      lastBlink1 = millis();
+      lastBlinkState1 = lastBlinkState1 == 0 ? 1 : 0;
+    } 
+  } */
+  for(int j = 0; j < nbrOfLeds; j++) {
+    digitalWrite(leds1[j], off());
+  }
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void blinkLightWinner2() {
+  /* for(int i = 0; i < 10; i++) {
+    if(millis() - lastBlink2 > 100 ) {
+      for(int j = 0; j < nbrOfLeds; j++) {
+        digitalWrite(leds2[j], lastBlinkState2 == 0 ? on() : off());
+      }
+      lastBlink2 = millis();
+      lastBlinkState2 = lastBlinkState2 == 0 ? 1 : 0;
+    } 
+  } */
+    for(int j = 0; j < nbrOfLeds; j++) {
+      digitalWrite(leds2[j], off());
+    }
+}
 
+void startGame() {
+  canPlay1 = 1;
+  canPlay2 = 1;
+}
+
+void readIncomingDatas() {
+  if(Serial.available() > 0){
+      String s = Serial.readStringUntil("\n");
+      Serial.println((String) "Received : " + s);
+
+      int delim = s.indexOf(':');
+      String command = s.substring(0,delim);
+
+      Serial.println("Command : " + command);
+
+      if(command == "start") {
+        startGame();
+      } 
+
+      if(command == "reset") {
+        setupGame();
+      }
+      
+  }
+}
+
+void sendCommand(String command) {
+  Serial.println((String) "c|" + command);
 }
--- frontend/sketch.js
+++ frontend/sketch.js
@@ -1,5 +1,5 @@
 let serial; // variable to hold an instance of the serialport library
-let portName; // fill in your serial port name here
+let portName = "/dev/tty.usbmodem1101"; // fill in your serial port name here
 
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -8,239 +8,293 @@ let millisecs = [];
 let secs = [];
 let mins = [];
 
+let errors = [0,0];
+
 let timers = [];
+
+let isInit = false;
+
+const PUNISHMENT = 2;
 
 const apiUrl = "https://arduino.test/api/";
 
 function setup() {
-    //createCanvas(width, height);
+  //createCanvas(width, height);
 
-    // serial
-    serial = new p5.SerialPort(); // make a new instance of the serialport library
-    serial.on("list", printList); // set a callback function for the serialport list event
-    serial.on("connected", serverConnected); // callback for connecting to the server
-    serial.on("open", portOpen); // callback for the port opening
-    serial.on("data", serialEvent); // callback for when new data arrives
-    serial.on("error", serialError); // callback for errors
-    serial.on("close", portClose); // callback for the port closing
+  // serial
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on("list", printList); // set a callback function for the serialport list event
+  serial.on("connected", serverConnected); // callback for connecting to the server
+  serial.on("open", portOpen); // callback for the port opening
+  serial.on("data", serialEvent); // callback for when new data arrives
+  serial.on("error", serialError); // callback for errors
+  serial.on("close", portClose); // callback for the port closing
 
-    serial.list(); // list the serial ports
-    serial.open(portName); // open a serial port
+  serial.list(); // list the serial ports
+  serial.open(portName); // open a serial port
 
-    init();
+  init();
 
-    fetchScores();
-
-    //remove element from dom
-    const main = document.getElementsByTagName('main');
-    main[0].remove();
+  //remove element from dom
+  const main = document.getElementsByTagName("main");
+  main[0].remove();
 }
 
 // get the list of ports:
 function printList(portList) {
-    // portList is an array of serial port names
-    for (let i = 0; i < portList.length; i++) {
-        console.log(i + ": " + portList[i]);
-    }
+  // portList is an array of serial port names
+  for (let i = 0; i < portList.length; i++) {
+    console.log(i + ": " + portList[i]);
+  }
 }
 
 function serverConnected() {
-    console.log("connected to server.");
+  console.log("connected to server.");
 }
 
 function portOpen() {
-    console.log("the serial port is opened.");
+  console.log("the serial port is opened.");
 }
 
-function serialEvent() {}
+function serialEvent() {
+  let data = serial.readLine();
+  if (data) {
+    console.log(data);
+    if(data.startsWith("c|")){
+      const datas = data.split("|");
+      const command = datas[1].split(":")[0];
+      const params = datas[1].split(":")[1].split("&");
+      console.log("Got command: " + command + " with params: " + params);
+     
+
+      parseCommand(command, params);
+    }
+  }
+}
 
 function serialError(err) {
-    console.log("Something went wrong with the serial port. " + err);
+  console.log("Something went wrong with the serial port. " + err);
 }
 
 function portClose() {
-    console.log("The serial port is closed.");
+  console.log("The serial port is closed.");
 }
 
-function draw() {}
+function draw() {
+  renderErrors();
+}
 
 function init() {
-    timers = [];
-    addChronos();
+  timers = [];
+  addChronos();
 }
 
 function addChronos() {
-    addChrono(0);
-    addChrono(1);
+  addChrono(0);
+  addChrono(1);
 }
 
 function startCounters() {
-    startCounter(0);
-    startCounter(1);
+  write("start");
+
+  startCounter(0);
+  startCounter(1);
+
 }
 
 function stopCounters() {
-    stopCounter(0);
-    stopCounter(1);
+  stopCounter(0);
+  stopCounter(1);
 }
 
 function startCounter(id) {
-    timer(id);
+  timer(id);
 }
 
 function stopCounter(id) {
-    clearTimeout(timers[id]);
+  clearTimeout(timers[id]);
 }
 
 function resetCounters() {
-    resetCounter(0);
-    resetCounter(1);
+  resetCounter(0);
+  resetCounter(1);
+  write("reset");
 }
 
 function resetCounter(id) {
-    clearTimeout(timers[id]);
-    mins[id] = 0;
-    secs[id] = 0;
-    millisecs[id] = 0;
-    document.getElementById('chrono' + id).innerHTML = "00:00:00";
+  clearTimeout(timers[id]);
+  mins[id] = 0;
+  secs[id] = 0;
+  millisecs[id] = 0;
+  document.getElementById("chrono" + id).innerHTML = "00:00:00";
 }
 
-
 function addChrono(id) {
-    let chrono = document.createElement("div");
-    chrono.id = "chrono" + id;
-    chrono.className = "chrono";
-    chrono.innerHTML = "00:00:00";
-    document.getElementById("section-chrono" + id).appendChild(chrono);
+  let chrono = document.createElement("div");
+  chrono.id = "chrono" + id;
+  chrono.className = "chrono";
+  chrono.innerHTML = "00:00:00";
+  document.getElementById("section-chrono" + id).appendChild(chrono);
 }
 
 function tick(id) {
-    millisecs[id]++;
-    if (millisecs[id] >= 60) {
-        millisecs[id] = 0;
-        secs[id]++;
-        if (secs[id] >= 60) {
-            secs[id] = 0;
-            mins[id]++;
-        }
+  millisecs[id]++;
+  if (millisecs[id] >= 60) {
+    millisecs[id] = 0;
+    secs[id]++;
+    if (secs[id] >= 60) {
+      secs[id] = 0;
+      mins[id]++;
     }
+  }
 }
 
 function add(id) {
-    mins[id] = mins[id] || 0;
-    secs[id] = secs[id] || 0;
-    millisecs[id] = millisecs[id] || 0;
-    tick(id);
-    document.getElementById('chrono' + id).innerHTML = (mins[id] > 9 ? mins[id] : "0" + mins[id]) +
-        ":" + (secs[id] > 9 ? secs[id] : "0" + secs[id]) +
-        ":" + (millisecs[id] > 9 ? millisecs[id] : "0" + millisecs[id]);
-    timer(id);
+  mins[id] = mins[id] || 0;
+  secs[id] = secs[id] || 0;
+  millisecs[id] = millisecs[id] || 0;
+  tick(id);
+  document.getElementById("chrono" + id).innerHTML =
+    (mins[id] > 9 ? mins[id] : "0" + mins[id]) +
+    ":" +
+    (secs[id] > 9 ? secs[id] : "0" + secs[id]) +
+    ":" +
+    (millisecs[id] > 9 ? millisecs[id] : "0" + millisecs[id]);
+  timer(id);
 }
 
 function timer(id) {
-    timers[id] = (setTimeout(() => add(id), 10));
+  timers[id] = setTimeout(() => add(id), 10);
 }
 
 async function getHttp(url) {
-    const res = await fetch(apiUrl + url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'apikey': 'AlrU5O9IyTmHw712sR8Wf2EisV0Ichd'
-        },
-    });
-    return await res.json();
+  const res = await fetch(apiUrl + url, {
+    headers: {
+      "Content-Type": "application/json",
+      apikey: "AlrU5O9IyTmHw712sR8Wf2EisV0Ichd",
+    },
+  });
+  return await res.json();
 }
 
 async function postHttp(url, data) {
-    const res = await fetch(apiUrl + url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept' : 'application/json',
-            'apikey' : 'AlrU5O9IyTmHw712sR8Wf2EisV0Ichd'
-        },
-        body: JSON.stringify(data),
-    });
+  const res = await fetch(apiUrl + url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      apikey: "AlrU5O9IyTmHw712sR8Wf2EisV0Ichd",
+    },
+    body: JSON.stringify(data),
+  });
 
-    return await res.json();
+  return await res.json();
 }
 
-
 async function fetchScores() {
+  const json = await getHttp("scores/top");
 
-    const json = await getHttp("scores/top");
+  console.log(json.data);
 
-    console.log(json.data);
-
-    renderTableTopScores(json.data);
+  renderTableTopScores(json.data);
 }
 
 function renderTableTopScores(datas) {
-    const section = document.getElementById('section-scores');
+  const section = document.getElementById("section-scores");
 
-    section.querySelector('#table-scores')?.remove();
+  section.querySelector("#table-scores")?.remove();
 
-    const table = document.createElement('table');
-    table.className = 'table';
-    table.id = 'table-scores';
+  const table = document.createElement("table");
+  table.className = "table";
+  table.id = "table-scores";
 
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
+  const thead = document.createElement("thead");
+  const tr = document.createElement("tr");
 
+  for (const mode of datas) {
+    const th = document.createElement("th");
+    th.innerHTML = mode[0].mode;
+    tr.appendChild(th);
+  }
+
+  const tbody = document.createElement("tbody");
+
+  for (let i = 0; i < 10; i++) {
+    const tr = document.createElement("tr");
     for (const mode of datas) {
-        const th = document.createElement('th');
-        th.innerHTML = mode[0].mode;
-        tr.appendChild(th);
+      const td = document.createElement("td");
+      const score = mode[i];
+      const divContent = document.createElement("div");
+      divContent.className = "td-content";
+      const pName = document.createElement("div");
+      pName.innerHTML = score?.player.name ?? "";
+      const pValue = document.createElement("div");
+      pValue.textContent = score?.value ?? "";
+
+      divContent.appendChild(pName);
+      divContent.appendChild(pValue);
+
+      td.appendChild(divContent);
+      tr.appendChild(td);
     }
+    tbody.appendChild(tr);
+  }
 
-    const tbody = document.createElement('tbody');
-
-    for (let i = 0; i < 10; i++) {
-        const tr = document.createElement('tr');
-        for (const mode of datas) {
-            const td = document.createElement('td');
-            const score = mode[i];
-            const divContent = document.createElement('div');
-            divContent.className = 'td-content';
-            const pName = document.createElement('div');
-            pName.innerHTML = score?.player.name ?? '';
-            const pValue = document.createElement('div');
-            pValue.textContent = score?.value ?? '';
-
-            divContent.appendChild(pName);
-            divContent.appendChild(pValue);
-
-            td.appendChild(divContent);
-            tr.appendChild(td);
-        }
-        tbody.appendChild(tr);
-    }
-
-
-
-
-
-    thead.appendChild(tr);
-    table.appendChild(tbody);
-    table.appendChild(thead);
-    section.appendChild(table);
-
-
+  thead.appendChild(tr);
+  table.appendChild(tbody);
+  table.appendChild(thead);
+  section.appendChild(table);
 }
 
 async function saveScore(name, value, mode) {
-    const data = {
-        name,
-        value,
-        "mode" : mode.toString()
-    };
-    try {
-        const res = await postHttp("scores", data);
-        console.log(res);
-        if(res) {
-            fetchScores();
-        }
-    }catch (e) {
-        console.log(e);
+  const data = {
+    name,
+    value,
+    mode: mode.toString(),
+  };
+  try {
+    const res = await postHttp("scores", data);
+    console.log(res);
+    if (res) {
+      fetchScores();
     }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//fetchScores();
+
+function write(command, params) {
+  const query = buildQuery(command, params);
+  console.log("sending: " + query);
+
+  serial.write(query + "\n");
+}
+
+function buildQuery(command, params) {
+  let query = command + ":";
+  if (params) {
+    query += params.join("&");
+  }
+  return query;
+}
+
+
+function parseCommand(command, params) {
+  if(command == "stop"){
+    stopCounter(params[0] - 1);
+  } 
+
+  if(command == "error"){
+    console.log("error: " + params[0]);
+    secs[params[0] - 1] += PUNISHMENT;
+    errors[params[0] - 1]++;
+  }
+}
+
+function renderErrors() {
+  for (let i = 0; i < 2; i++) {
+    document.getElementById("errors_" + i).innerHTML = "Erreurs : " + errors[i];
+  }
 }
